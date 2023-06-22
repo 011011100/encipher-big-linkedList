354121eada013c0ddd49602e0ac748944c75f033
Administrator <1401383704@qq.com>
2023-06-22 11:55:16 +0800

    初始化

---
+++ src/main/java/com/lzh/encipherbiglinkedlist/EncipherBigLinkedListApplication.java
@@ -0,0 +1,13 @@
+package com.lzh.encipherbiglinkedlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncipherBigLinkedListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncipherBigLinkedListApplication.class, args);
+    }
+
+}
