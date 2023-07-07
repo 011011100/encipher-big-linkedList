2d5c48696c32b4982d578eee4035300f152e4ba4
Administrator <1401383704@qq.com>
2023-07-07 15:23:57 +0800

    创建 加密方法

---
+++ src/main/java/com/lzh/encipherbiglinkedlist/block/utils/SHAUtils.java
@@ -0,0 +1,31 @@
+package com.lzh.encipherbiglinkedlist.block.utils;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+/**
+ * <p>
+ * SHA加密<br>
+ * </p>
+ *
+ * @author LZH
+ * @version V1.0
+ * @since 2023/7/7 15:02
+ */
+public class SHAUtils {
+
+    private static final String KEY_SHA = "SHA";
+
+    public static String encodeSHA(String content){
+        byte[] inputData = content.getBytes();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(KEY_SHA);
+            messageDigest.update(inputData);
+            BigInteger sha = new BigInteger(messageDigest.digest());
+            return sha.toString(256);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
